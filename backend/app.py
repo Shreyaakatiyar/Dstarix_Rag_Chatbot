52d67fc4a98fb2ff941ce1ec4b2374a924e02df4
Shreyaa Katiyar <shreyaakatiyar@gmail.com>
2026-07-23 23:09:01 +0530

    Connect gemini to backend

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from services.gemini_service import test_gemini
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DStarix RAG Chatbot Backend is Running!"
+    return "Backend Running"
+
+@app.route("/test-gemini")
+def gemini():
+    return test_gemini()
 
 if __name__ == "__main__" :
     app.run(debug=True)
---
+++ backend/services/gemini_service.py
@@ -0,0 +1,12 @@
+from google import genai
+from config import GEMINI_API_KEY
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+def test_gemini():
+    response = client.models.generate_content(
+        model="gemini-3.1-flash-lite",
+        contents="Say only: Gemini Connected"
+    )
+
+    return response.text
