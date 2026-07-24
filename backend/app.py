58619cce6233bd506d2240107cb94626c1ed6ff5
Shreyaa Katiyar <shreyaakatiyar@gmail.com>
2026-07-24 15:22:39 +0530

    Implement Rag functionality

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,5 @@
-from flask import Flask
-from services.gemini_service import test_gemini
-from services.vector_service import (
-    load_documents,
-    split_documents,
-    create_vector_store,
-    retrieve_documents,
-)
+from flask import Flask, request
+from services.gemini_service import ask_question
 
 app = Flask(__name__)
 
@@ -13,28 +7,20 @@ app = Flask(__name__)
 def home():
     return "Backend Running"
 
-@app.route("/test-gemini")
-def gemini():
-    return test_gemini()
+@app.route("/chat", methods=["POST"])
+def chat():
+    
+    data = request.get_json()
 
-@app.route("/build-db")
-def build_db():
-    docs = load_documents()
-    chunks = split_documents(docs)
-    create_vector_store(chunks)
+    question = data.get("question")
 
-    return "Vector database created successfully!"
+    answer = ask_question(question)
 
-@app.route("/test-retriever")
-def test_retriever():
-    docs = retrieve_documents("What is the internship duration?")
 
-    result = ""
-
-    for doc in docs:
-        result +=doc.page_content + "\n\n"
-
-    return result
+    return{
+        "question": question,
+        "answer": answer
+    }
 
 if __name__ == "__main__" :
     app.run(debug=True)
--- backend/services/gemini_service.py
+++ backend/services/gemini_service.py
@@ -1,5 +1,7 @@
 from config import GEMINI_API_KEY
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from services.vector_service import retrieve_documents
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-3.1-flash-lite",
@@ -7,6 +9,33 @@ llm = ChatGoogleGenerativeAI(
     temperature=0
 )
 
-def test_gemini():
-    response = llm.invoke("Say Only: Gemini Connected")
+prompt = ChatPromptTemplate.from_template(""" 
+You are an AI Internship Guide Assisstant.
+
+Answer ONLY using the provided context.
+
+If the answer is not available in the context, say:
+"I couldn't find that information in the internship guide."
+
+Context:
+{context}
+
+Question:
+{question}
+""")
+
+def ask_question(question: str):
+    documents = retrieve_documents(question)
+
+    context = "\n\n".join(
+        doc.page_content for doc in documents
+    )
+
+    message = prompt.format_messages(
+        context=context,
+        question=question
+    )
+
+    response = llm.invoke(message)
+
     return response.content
